cf748e1ea79badcf77c1a4bda6ed39ededf44d9f
BrackenLo <BrackenLo@proton.me>
2024-07-02 12:55:29 +0100

    Moved player_inner into it's own file
    Moved the player_inner class from player into it's own file. Also going
    to move all new player classes into this file as well (e.g. player_avian)
    
    Also changed avian to set max fall speed instead of gravity on space held

--- src/game/player.h
+++ src/game/player.h
@@ -1,82 +1,9 @@
 #pragma once
 #include "../engine/entity.h"
 #include "../engine/save.h"
+#include "player_inner.h"
 #include "raylib.h"
 #include <vector>
-
-enum class PlayerType {
-    Base,
-    Avian,
-};
-
-//====================================================================
-
-class PlayerInner : public IDebug {
-public:
-    friend class Player;
-
-    PlayerInner(class Player* outer);
-
-protected:
-    virtual void update(class World* world);
-    virtual void fixed_update(class World* world, float dt);
-    virtual void render(class World* world);
-
-    virtual bool do_jump(class World* world, float dt);
-    virtual bool do_wall_jump(class World* world, float dt);
-    virtual void on_grounded(class World* world, float dt);
-    virtual void on_ceiling(class World* world, float dt);
-    virtual void on_wall(class World* world, float dt);
-
-    virtual void do_ability1(class World* world, float dt);
-    virtual void do_ability2(class World* world, float dt);
-
-protected:
-    virtual void update_jump_variables();
-    virtual float get_gravity(class World* world);
-
-    inline PlayerType get_player_type() { return player_type; }
-
-protected:
-    PlayerType player_type;
-
-    // Config Player Variables
-    float accel;
-    float deaccel;
-
-    float max_velocity_x;
-    float max_fall_speed;
-
-    float jump_buffer_size;
-    float coyote_time;
-
-    float jump_height;
-    float jump_time_to_peak;
-    float jump_time_to_descent;
-    float variable_jump_height;
-    int total_jumps;
-
-    float wall_slide_gravity;
-    float wall_jump_impulse_x; // TODO - figure out a better algorithm (horizontal and vertical)
-
-    Color player_color;
-
-protected:
-    // Managed player variables
-    class Player* outer;
-
-    float jump_impulse;
-    float jump_gravity;
-    float fall_gravity;
-    float variable_jump_gravity;
-
-public:
-    // IDebug functionality
-    virtual const char* get_name() override { return "player_inner"; }
-    virtual void get_properties(std::vector<DebugProperty>* properties) override;
-};
-
-//====================================================================
 
 //====================================================================
 
